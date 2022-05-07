4bba00a1ab7a3c788d3325d694ca347995220c3f
Shailseen <shailesh13246@gmail.com>
2022-05-07 09:53:33 +0530

    fix: bug

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import Navbar from "./components/Navbar/Navbar";
 
 function App() {
   return (
-    <div className="App">
+    <div className={styles.App}>
       <div className={styles.navbar}>
         <Navbar />
       </div>
