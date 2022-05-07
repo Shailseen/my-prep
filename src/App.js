c746eb44b791115306b3badb797ad08c22a37fb1
Shailseen <shailesh13246@gmail.com>
2022-05-07 11:37:26 +0530

    fix: auth-bug

--- src/App.js
+++ src/App.js
@@ -4,6 +4,19 @@ import { ToastContainer } from "react-toastify";
 import styles from "./App.module.css";
 import Navbar from "./components/Navbar/Navbar";
 
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyB_buVcpv0maTgwt7MDjHM6ux0BFIUUg24",
+  authDomain: "neogqod.firebaseapp.com",
+  projectId: "neogqod",
+  storageBucket: "neogqod.appspot.com",
+  messagingSenderId: "4959195096",
+  appId: "1:4959195096:web:c99195a7b4412c4fceacad",
+  measurementId: "G-KZKGG29D9E",
+};
+initializeApp(firebaseConfig);
+
 function App() {
   return (
     <div className="App">
